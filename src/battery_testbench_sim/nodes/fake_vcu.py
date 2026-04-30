9ea2b7c021612a7dd28aa648b29462cba1cb5b4c
Jiaxing Lu <louischemnitz@gmail.com>
2026-04-30 16:40:35 +0200

    feat: log VCU state transitions

--- src/battery_testbench_sim/nodes/fake_vcu.py
+++ src/battery_testbench_sim/nodes/fake_vcu.py
@@ -1,3 +1,9 @@
+import logging
+
+
+logger = logging.getLogger(__name__)
+
+
 class FakeVCU:
     def __init__(self):
         self.last_state = None
@@ -10,9 +16,16 @@ class FakeVCU:
         else:
             new_state = 1  # normal
 
+        if self.last_state is not None and self.last_state != new_state:
+            logger.warning(
+                "VCU state transition: %d -> %d due to SOC=%d %%",
+                self.last_state,
+                new_state,
+                soc,
+            )
+
         self.last_state = new_state
 
         modified = dict(data)
         modified["state"] = new_state
-
         return modified
