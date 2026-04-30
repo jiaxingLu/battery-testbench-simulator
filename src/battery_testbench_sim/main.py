fe85648253573575224636bb52bc557b491ae983
Jiaxing Lu <louischemnitz@gmail.com>
2026-04-30 16:31:02 +0200

    refactor: move runtime loop into supervisor

--- src/battery_testbench_sim/main.py
+++ src/battery_testbench_sim/main.py
@@ -1,49 +1,40 @@
 import logging
-import time
 
+from battery_testbench_sim.can_bus import CanBus
 from battery_testbench_sim.config import load_config
 from battery_testbench_sim.faults.fault_injector import FaultInjector
 from battery_testbench_sim.logging_setup import setup_logging
-from battery_testbench_sim.messages.bms_status import encode_bms_status
 from battery_testbench_sim.nodes.fake_bms import FakeBMS
 from battery_testbench_sim.nodes.verifier import Verifier
+from battery_testbench_sim.runtime.supervisor import Supervisor
 
 
 def main():
     setup_logging()
     logger = logging.getLogger(__name__)
 
-    config = load_config("configs/bms_default.yaml")
-    bms_cfg = config["bms"]
-    fault_cfg = config.get("fault", {})
+    bms_config = load_config("configs/bms_default.yaml")
 
-    bms = FakeBMS(
-        pack_voltage=bms_cfg["pack_voltage"],
-        pack_current=bms_cfg["pack_current"],
-        soc=bms_cfg["soc"],
-        state=bms_cfg["state"],
-        fault_level=bms_cfg["fault_level"],
-        cycle_time_s=bms_cfg["cycle_time_s"],
+    bms_cfg = bms_config["bms"]
+    fault_cfg = bms_config.get("fault", {})
+
+    bms = FakeBMS(**bms_cfg)
+    verifier = Verifier()
+    fault_injector = FaultInjector(fault_cfg)
+
+    bus_tx = CanBus(interface="virtual", channel="testbench_runtime")
+    bus_rx = CanBus(interface="virtual", channel="testbench_runtime")
+
+    supervisor = Supervisor(
+        bms=bms,
+        verifier=verifier,
+        bus_tx=bus_tx,
+        bus_rx=bus_rx,
+        fault_injector=fault_injector,
+        bms_status_id=0x180,
     )
 
-    fault_injector = FaultInjector(fault_cfg)
-    verifier = Verifier()
-
-    cycle_count = 0
-
-    try:
-        while True:
-            data = bms.get_status_data()
-            data = fault_injector.apply(data, cycle_count)
-            frame = encode_bms_status(data)
-
-            verifier.log_status(frame)
-
-            cycle_count += 1
-            time.sleep(bms.cycle_time_s)
-
-    except KeyboardInterrupt:
-        logger.info("System stopped by user.")
+    supervisor.run()
 
 
 if __name__ == "__main__":
---
+++ src/battery_testbench_sim/runtime/supervisor.py
@@ -0,0 +1,57 @@
+from battery_testbench_sim.messages.bms_status import encode_bms_status
+import logging
+import time
+
+
+logger = logging.getLogger(__name__)
+
+
+class Supervisor:
+    def __init__(
+        self,
+        bms,
+        verifier,
+        bus_tx,
+        bus_rx,
+        fault_injector,
+        bms_status_id,
+    ):
+        self.bms = bms
+        self.verifier = verifier
+        self.bus_tx = bus_tx
+        self.bus_rx = bus_rx
+        self.fault_injector = fault_injector
+        self.bms_status_id = bms_status_id
+
+        self.cycle_count = 0
+
+    def run(self):
+        try:
+            while True:
+                data = self.bms.get_status_data()
+                data = self.fault_injector.apply(data, self.cycle_count)
+
+                frame = encode_bms_status(data)
+
+                self.bus_tx.send(
+                    arbitration_id=self.bms_status_id,
+                    data=frame,
+                )
+
+                msg = self.bus_rx.recv(timeout_s=1.0)
+
+                if msg is None:
+                    logger.warning("No CAN message received.")
+                elif msg.arbitration_id == self.bms_status_id:
+                    self.verifier.log_status(bytes(msg.data))
+                else:
+                    logger.warning("Unexpected CAN ID: 0x%X", msg.arbitration_id)
+
+                self.cycle_count += 1
+                time.sleep(self.bms.cycle_time_s)
+
+        except KeyboardInterrupt:
+            logger.info("System stopped by user.")
+        finally:
+            self.bus_tx.shutdown()
+            self.bus_rx.shutdown()
